087433b68032ddcf97234bbb6404c8c60e2455f2
igorsobik3-cmyk <igorsobik3@gmail.com>
2026-02-06 15:14:05 +0100

    Delete max_tab.py

--- max_tab.py
+++
@@ -1,9 +0,0 @@
-tab = [4, 6, 2, 1, 3]
-
-max_value = tab[0]
-
-for i in range(1, len(tab)):
-    if tab[i] > max_value:
-        max_value = tab[i]
-
-print("Maksymalna wartosc w tablicy:", max_value)
